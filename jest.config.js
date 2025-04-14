aaea5fe30cb284e3fc4f2e6144c9c983cae6bed3
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-04-14 15:59:03 -0300

    config(jest): config jest to show the coverage of tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   testEnvironment: "node",
+  collectCoverage: true,
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
+  coverageDirectory: "coverage",
+  coverageReporters: ["html", "text-summary", "lcov"]
 };
